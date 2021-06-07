5c917c1e8e4491d8edd9281d80f43f235220b420
Alan <alanpalazon@gmail.com>
2021-06-08 00:42:10 +0100

    js chnaged

--- script.js
+++ script.js
@@ -1,49 +1,35 @@
-//function calls and onload functionality
-
+//global vars
 const gridCont = document.getElementById("grid-cont");
-window.addEventListener('load',createGrid(randomGridNum()));
+let gridCells = 100;
 
+//on page load 
 
-//add if statement for to check if user has craeated a grid and then load that grid
-//instead of default to window  
+    //grid 
+gridCont.style.gridTemplateColumns = `repeat(${ Math.sqrt(gridCells)}, 1fr)`;
+gridCont.style.gridTemplateRows = `repeat(${ Math.sqrt(gridCells)}, 1fr)`;
+for(let i = 0; i < gridCells; i++){
+    const gridKid = document.createElement('div');
+    gridKid.classList.add('grid-child');
+    gridKid.setAttribute("id", i);
+    gridCont.appendChild(gridKid);        
+    } 
+    //colour
+gridCont.childNodes.forEach(child => child.addEventListener('mouseover', function(){
+        child.style.backgroundColor = "black"}));
 
+//when rainbow clicked
+document.getElementById('rainbow').addEventListener('click', function(){
+    gridCont.childNodes.forEach(child => child.addEventListener('mouseover', function(){
+        child.style.backgroundColor = 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';}));
+});
 
+//when eraser clicked
+document.getElementById('white').addEventListener('click', function(){
+    gridCont.childNodes.forEach(child => child.addEventListener('mouseover', function(){
+        child.style.backgroundColor = 'White';}));
+});
 
-//functions 
-
-
-//not creating rows - fix!
-function createGrid(gridCells){ 
-    gridCont.style.gridTemplateColumns = `repeat(${ Math.sqrt(gridCells)}, 1fr)`;
-    gridCont.style.gridTemplateRows = `repeat(${ Math.sqrt(gridCells)}, 1fr)`;
-    for(let i = 0; i < gridCells; i++){
-        const gridKid = document.createElement('div');
-        gridKid.classList.add('grid-child');
-        gridKid.setAttribute("id", i);
-        gridCont.appendChild(gridKid);        
-    }  
-}
-
-function mouseOverColour(targetElem, colourChange){
-    //add if statement to check whether div created by user or default 
-    let randColour = Math.floor(Math.random() * 256);
-     const targetElemChild = document.getElementById(targetElem).childNodes;
-     targetElemChild.forEach(child => child.addEventListener('mouseover', function(){
-        child.style.backgroundColor= colourChange;
-     }));
-}
-
-mouseOverColour("grid-cont", randomHsl());
-
-function randomHsl() {
-    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
-}
-
-function randomGridNum(){
-    let newRandNum = Math.floor(Math.random()*(16 - 2) + 2);
-    const gridColsRows = newRandNum**2;
-    return gridColsRows;
-}
-
-
-
+//when reset clicked
+document.getElementById('clear').addEventListener('click', function(){
+    gridCont.children.style.backgroundColor == "white";
+});
