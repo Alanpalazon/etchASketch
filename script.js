904d95986fedf49f138207a5b0aa57a6db9d9ce8
Alan <alanpalazon@gmail.com>
2021-06-12 23:53:35 +0100

    need to create form functionality

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const gridCont = document.getElementById('grid-cont');
 const gridChild = gridCont.childNodes;
 
-//Problem: this function means cols/rows have fixed widths/heights that won't change 
-//when user creates a c custom grid - FIX!
 function createGrid(gridRows, gridCols){
     gridCont.style.gridTemplateColumns = `repeat(${ gridCols}, 1fr)`;
     gridCont.style.gridTemplateRows = `repeat(${ gridRows}, 1fr)`;
@@ -14,32 +12,47 @@ function createGrid(gridRows, gridCols){
         } 
 }
 
-createGrid(11, 11);
+createGrid(20, 20);
 
+function gridColourChange(){
+    for(let j = 0; j < gridChild.length; j++){
+        gridChild[j].style.backgroundColor = "white";
+    }
+}
+
+// createmouseover colour change functiuion 
+
+function mouseOverColour(colourParam){
+    gridChild.forEach(child => child.addEventListener('mouseover', function(){
+        child.style.backgroundColor = colourParam;}));
+}
 
 function buttonClicks(e){
     if(e.target.id == "rainbow"){
             gridChild.forEach(child => child.addEventListener('mouseover', function(){
             child.style.backgroundColor = 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';}));
             return;
-    }else if(e.target.id == 'white'){
-            gridChild.forEach(child => child.addEventListener('mouseover', function(){
-            child.style.backgroundColor = 'white';}));
-            return;
-    }
+        }
+    if(e.target.id == 'white'){mouseOverColour("white"); return;}
+    if(e.target.id == 'clear'){
+        for(let i = gridChild.length; i > 100; i--){
+            gridCont.removeChild(gridChild[i-1]);
+        }
+        gridCont.style.gridTemplateColumns = `repeat(10, 1fr)`;
+        gridCont.style.gridTemplateRows = `repeat(10, 1fr)`;
+        gridColourChange();        
+    }       
 }
+
 
 
 
 document.getElementById('rainbow').addEventListener('click', buttonClicks);
 document.getElementById('white').addEventListener('click', buttonClicks);
-document.getElementById('clear').addEventListener('click', function(){
-    gridChild.forEach(child => child.style.backgroundColor == "white");
-    for(let i = gridChild.length; i > 100; i--){
-        console.log(i);
-        gridCont.removeChild(gridChild[i-1]);
-    }
-
-});
+document.getElementById('clear').addEventListener('click', buttonClicks);
+//create button 
+    // use number input for grid/cols 
+    //move colour inout to button group 
+         // use colour input as mouseOverColour()
 
 
